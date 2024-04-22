2927072d11974f5f16e3fbc3e77f73171171df43
Sofie Jans <sjans@techfak.uni-bielefeld.de>
2024-04-22 11:15:22 +0200

    added Map to Page

--- src/main/vue/router/index.js
+++ src/main/vue/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StartView from '../views/Start.vue'
+import MapView from '../views/Map.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: StartView
     },
+      {
+          path: '/map',
+          name: 'map',
+          component: MapView
+      }
+
   ]
 })
 
