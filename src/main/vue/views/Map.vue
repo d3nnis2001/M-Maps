9a13ca3a80385bf6fa051f91772720f458784631
Sofie Jans <sjans@techfak.uni-bielefeld.de>
2024-04-24 16:05:46 +0200

    refs #76993 @0:30 added openrailwaymap layer and example marker

--- src/main/vue/views/Map.vue
+++ src/main/vue/views/Map.vue
@@ -10,6 +10,18 @@ onMounted(() => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    new L.TileLayer('http://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png',
+        {
+            attribution: '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>, Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> and OpenStreetMap',
+            minZoom: 2,
+            maxZoom: 19,
+            tileSize: 256
+        }).addTo(map);
+
+    L.marker([52.0368609707235, 8.49525289846377]).addTo(map)
+        .bindPopup('This is an example popup<br> for a marker.')
+        .openPopup();
 })
 
 </script>
