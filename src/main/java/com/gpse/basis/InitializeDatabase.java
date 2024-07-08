cefe260b768dd1e866f21a8eb74a91a32fb49ee9
Maurice Meise <mameise@techfak.uni-bielefeld.de>
2024-07-09 00:15:25 +0200

    InitializeDatabase fix

--- src/main/java/com/gpse/basis/InitializeDatabase.java
+++ src/main/java/com/gpse/basis/InitializeDatabase.java
@@ -12,26 +12,49 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Database for testing.
+ */
 @Service
 @Profile("Name1")
 public class InitializeDatabase implements InitializingBean {
+    /**
+     * BERLIN_OSTBAHNHOF.
+     */
+    public static final String BERLIN_OSTBAHNHOF = "Berlin Ostbahnhof";
+    /**
+     * DB_REGIO_SCHIENE_NORD_OST_NO.
+     */
+    public static final String DB_REGIO_SCHIENE_NORD_OST_NO = "DB Regio Schiene Nord-Ost (NO)";
+    /**
+     * GLEISLAGEDATEN.
+     */
+    public static final String GLEISLAGEDATEN = "Gleislagedaten";
+    /**
+     * HOCH.
+     */
+    public static final String HOCH = "hoch";
     private static final String PRIMARY_AND_DARK_COLOR = "#282D37";
-    private final UserRepository usRepo;
     private final InspectionOrderRepository ioRepo;
-    private final ReparaturRepository reRepo;
     private final ChecklistRepository checkRepo;
     private final GleisLageRangeRepository glrRepo;
     private final SettingsRepository settingsRepository;
 
     private final GeoTrackData geoTrackRepository;
+    private final UserRepository usRepo;
 
+    /**
+     * InitializeDatabase.
+     */
+    @SuppressWarnings("checkstyle:JavadocMethod")
     @Autowired
-    public InitializeDatabase(final UserRepository usRepo, final InspectionOrderRepository ioRepo, final ReparaturRepository reRepo,
-                              final ChecklistRepository checkRepo, final GleisLageRangeRepository r, final GeoTrackData gTD, final SettingsRepository settingsRepository) {
+    public InitializeDatabase(final UserRepository usRepo, final InspectionOrderRepository ioRepo,
+                              final ChecklistRepository checkRepo,
+                              final GleisLageRangeRepository r, final GeoTrackData gTD,
+                              final SettingsRepository settingsRepository) {
 
         this.usRepo = usRepo;
         this.ioRepo = ioRepo;
-        this.reRepo = reRepo;
         this.checkRepo = checkRepo;
         this.glrRepo = r;
         this.geoTrackRepository = gTD;
@@ -45,29 +68,30 @@ public class InitializeDatabase implements InitializingBean {
         initRanges();
         initGeoTrack();
         initSettings();
-        initInspectionOrder();;
+        initInspectionOrder();
         initChecklistTemplates();
     }
 
+    /**
+     * Initialize users for testing.
+     */
     public void initUsers() {
         // Test User 1
         UserModel user = new UserModel("d3nnis.s@web.de", "hello", "Georg", "Bauer");
         user.addRole("Prüfer");
         user.addRole("Administrator");
-        UserModel user2 = new UserModel("mauricemeise@gmx.net", "asdf", "Jochen", "Bauer");
+        UserModel user2 = new UserModel("mauricemeise@gmx.net", "asdf", "Jochen", "Bau");
         user2.addRole("Bearbeiter");
-        user2.addRole("Datenverwalter");
         UserModel user3 = new UserModel("affe@web.de", "affe", "Charlie", "Monkey");
-        user3.addRole("Prüfer");
         user3.addRole("Datenverwalter");
-        UserModel user4 = new UserModel("test", "abc", "Hi", "Du");
-        user4.addRole("Bearbeiter");
         usRepo.save(user);
         usRepo.save(user2);
         usRepo.save(user3);
-        usRepo.save(user4);
     }
 
+    /**
+     * Initialize checklists for testing.
+     */
     public void initChecklists() {
         ArrayList<String> items = new ArrayList<>();
         items.add("Checker 1");
@@ -88,6 +112,9 @@ public class InitializeDatabase implements InitializingBean {
         checkRepo.save(check1);
     }
 
+    /**
+     * Initialize ranges for testing.
+     */
     public void initRanges() {
         glrRepo.deleteAll();
         GleisLageRange range1 = new GleisLageRange(GleisLageRange.Level.SRA, 12, 10, 8, 6, 5);
@@ -98,19 +125,25 @@ public class InitializeDatabase implements InitializingBean {
         glrRepo.save(range3);
     }
 
+    /**
+     * Initialize inspection orders for testing.
+     */
     public void initInspectionOrder() {
         InspectionOrder inspec1 = new InspectionOrder("p-1717767131183662",
-            "1010", "", "Bielefeld Hbf", "Berlin Ostbahnhof",
-            "2024/07/12", "2024/07/17", "DB Regio Schiene Nord-Ost (NO)",
-            "Gleislagedaten", "in Bearbeitung", "Dringend!", false, "hoch");
+            "1010", "", "Bielefeld Hbf", BERLIN_OSTBAHNHOF,
+            "2024/07/12", "2024/07/17", DB_REGIO_SCHIENE_NORD_OST_NO,
+            GLEISLAGEDATEN, "in Bearbeitung", "Dringend!", false, HOCH);
         InspectionOrder inspec2 = new InspectionOrder("p-1718015853290597",
-            "1020", "", "Hamburg Hbf", "Berlin Ostbahnhof",
-            "2024/09/12", "2024/09/17", "DB Regio Schiene Nord-Ost (NO)",
-            "Gleislagedaten", "beauftrage", "", false, "hoch");
+            "1020", "", "Hamburg Hbf", BERLIN_OSTBAHNHOF,
+            "2024/09/12", "2024/09/17", DB_REGIO_SCHIENE_NORD_OST_NO,
+            GLEISLAGEDATEN, "beauftrage", "", false, HOCH);
         ioRepo.save(inspec1);
         ioRepo.save(inspec2);
     }
 
+    /**
+     * Initialize settings for testing.
+     */
     private void initSettings() {
         Settings settings = new Settings("", new Colors(PRIMARY_AND_DARK_COLOR, "#ec0016",
             "#1e7f5e", "#e21437", "#fec705", PRIMARY_AND_DARK_COLOR, "#31CCEC"),
@@ -118,7 +151,9 @@ public class InitializeDatabase implements InitializingBean {
         settingsRepository.save(settings);
     }
 
-
+    /**
+     * Initialize geotrack for testing.
+     */
     public void initGeoTrack() {
         Iterable<GeoData> lst = geoTrackRepository.findAll();
         AtomicBoolean found = new AtomicBoolean(false);
@@ -127,9 +162,14 @@ public class InitializeDatabase implements InitializingBean {
                 found.set(true);
             }
         });
-        if (!found.get())
+        if (!found.get()) {
             geoTrackRepository.save(new GeoData(1, 52.17027, 9.08446, 0, "1"));
+        }
     }
+
+    /**
+     * Initialize checklisttemplates for testing.
+     */
     public void initChecklistTemplates() {
         // Test Checklist Template 1
         LinkedList<String> tasks = new LinkedList<>(Arrays.asList("Punkt 1", "Punkt 2", "Punkt 3"));
